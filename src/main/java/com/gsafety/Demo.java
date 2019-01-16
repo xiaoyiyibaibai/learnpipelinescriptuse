cada5c14a45edcc004a7a26df8f2a6ba9f89f7ef
xiaoyiyibaibai <2367808698@qq.com>
2019-01-16 09:23:40 +0800

    test

---
+++ src/main/java/com/gsafety/Demo.java
@@ -0,0 +1,14 @@
+package com.gsafety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@EnableAutoConfiguration
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class Demo {
+	public static void main(String[] args) {
+		SpringApplication.run(Demo.class, args);
+	}
+}
